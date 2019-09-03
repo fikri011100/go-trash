c309c638fbcae7e706ed4949a45591a4cd224d6f
Rofiif Zufar Allam <zufaralam02@gmail.com>
2019-09-03 16:09:32 +0700

    variable

--- main.go
+++ main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	//var x int
+	//fmt.Println(x)
+
+	var x int = 5
+	var y int = 3
+	var sum int = x + y
+	var name string = "Allam"
+	var isLoading bool = false
+	var floatNumber float64 = 3.5
+
+	z := 10
+	address := "south tangerang"
+
+
+	fmt.Println(sum)
+	fmt.Println(name)
+	fmt.Println(isLoading)
+	fmt.Println(floatNumber)
+
+	fmt.Println(z)
+	fmt.Println(address)
 }
