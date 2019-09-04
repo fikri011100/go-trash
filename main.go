95de1c9f3eb6063b5d978ebf89f8a5c3f37aae5f
Rofiif Zufar Allam <zufaralam02@gmail.com>
2019-09-04 14:20:15 +0700

    if else conditional

--- main.go
+++ main.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// fix length array
-	//var arr [3]int
-	//arr[2] = 2
-	//fmt.Println(arr)
-
-	// short hand fix array
-	//arr := [3]int {1, 2, 3}
-	//fmt.Println(arr)
-
-	// short hand dynamic array
-	arr := []int {1, 2, 3}
-	arr = append(arr, 4)
-	fmt.Println(arr)
-
-
+	x := 4
+	if x < 3 {
+		fmt.Println("Ayeee")
+	} else if x == 4 {
+		fmt.Println("Yesss")
+	} else {
+		fmt.Println("Ooops")
+	}
 }
