235ed47d8483fbe57d5030adac392caa46f010e0
Rofiif Zufar Allam <zufaralam02@gmail.com>
2019-09-04 21:35:32 +0700

    pointer

--- main.go
+++ main.go
@@ -1,32 +1,16 @@
+// & = memory, * = value
 package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
+func main() {
+	i := 10
+	inc(&i)
+
+	//fmt.Println(&i)
+	fmt.Println(i)
 }
 
-func main() {
-	//p := Person{Name: "Ega", Age: 28}
-	//
-	//fmt.Println(p)
-	//fmt.Println(p.Name)
-
-	// array of struct
-	p := []Person{
-		{Name: "Ega", Age: 28},
-		{Name: "Yuni", Age: 21},
-	}
-	//
-	//fmt.Println(p)
-	//fmt.Println(p[1])
-	//fmt.Println(p[1].Age)
-
-	//traverse over array of struct
-	for _, elem := range p {
-		fmt.Println("name", elem.Name)
-		fmt.Println("age", elem.Age)
-		fmt.Println("")
-	}
+func inc(x *int) {
+	*x++
 }
